8781b121b8ae92710e3380c948278dc7023804e2
CMEL123 <dimaver.96@yandex.ru>
2025-04-07 00:45:39 +0300

    Этап 1. Сервис статистики

---
+++ stats/server/src/main/java/ru/practicum/StatsService.java
@@ -0,0 +1,37 @@
+
+package ru.practicum;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StatsService {
+
+    private final StatsRepository statsRepository;
+
+    @Transactional
+    public void create(EndpointHitDto dto) {
+        statsRepository.save(EndpointDtoMapper.toEntity(dto));
+    }
+
+    public List<StatsDto> findStats(LocalDateTime start, LocalDateTime end,
+                                               List<String> uris, String unique) {
+
+        if (unique.equals("true")) {
+            return statsRepository.findStatUnique(start, end, uris);
+        } else {
+            return statsRepository.findStat(start, end, uris);
+
+        }
+
+    }
+}
