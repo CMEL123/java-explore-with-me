fe790c55067cf27a6146def0c00fd0f9155cdfd3
CMEL123 <dimaver.96@yandex.ru>
2025-04-14 02:02:09 +0300

    Замечание от Git

--- ewm-service/src/main/java/ru/practicum/event/dto/EventRequestStatusUpdateResult.java
+++ ewm-service/src/main/java/ru/practicum/event/dto/EventRequestStatusUpdateResult.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import ru.practicum.request.dto.ParticipationRequestDto;
+import ru.practicum.request.ParticipationRequestDto;
 
 import java.util.List;
 
